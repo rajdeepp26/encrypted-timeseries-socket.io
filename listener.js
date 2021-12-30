9ee4ce95c19a2a8da1baf6d9b0cb13599bbef220
Rajdeep Purkayastha <rajdeep.purkayastha26@gmail.com>
2021-12-30 08:02:23 +0530

    moved db connection to seperate file

--- listener.js
+++ listener.js
@@ -4,29 +4,14 @@ let express = require("express");
 let app = express();
 let http = require("http").Server(app);
 let listenerServer = require("socket.io")(http);
-const mongoose = require("mongoose");
 
 let LISTENER_PORT = process.env.LISTENER_PORT || 3000;
 const HOST = process.env.HOST;
 
 const listenerService = require("./services/listenerService");
-const USER_NAME = process.env.USER_NAME;
-const USER_PASSWORD = process.env.USER_PASSWORD;
-const MONGO_DB = process.env.MONGO_DB;
+const databaseConnect = require("./db-config/databaseConnect");
 
-const DB = `mongodb+srv://${USER_NAME}:${USER_PASSWORD}@encrypted-timeseries.nxkbi.mongodb.net/${MONGO_DB}?retryWrites=true&w=majority`;
-
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-  })
-  .then((con) => {
-    console.log("Connection with DB has been established successfully.");
-  })
-  .catch((err) => {
-    console.log("Connect failed to establish", err);
-  });
-
+databaseConnect();
 app.use(express.static(__dirname + "/public"));
 
 // Connect listener
@@ -37,7 +22,9 @@ listenerServer.on("connection", function (socket) {
     console.log(message);
     try {
       let encryptedObject = message.toString().slice(0, -1);
-      let decryptedObject = await listenerService.getDecryptedObject(encryptedObject);
+      let decryptedObject = await listenerService.getDecryptedObject(
+        encryptedObject
+      );
       let validObject = await listenerService.validateObject(decryptedObject);
       if (Object.keys(validObject).length === 3) {
         let dbResponse = await listenerService.saveToDb(validObject);
